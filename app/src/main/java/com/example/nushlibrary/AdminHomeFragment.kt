ca046e31be9158c72a0defcf969061923ff2f21c
mug1wara26 <aloysiusgsq@gmail.com>
2021-03-21 22:49:30 +0800

    Working on dialog box to prompt admin to add book

--- app/src/main/java/com/example/nushlibrary/AdminHomeFragment.kt
+++ app/src/main/java/com/example/nushlibrary/AdminHomeFragment.kt
@@ -2,6 +2,7 @@ package com.example.nushlibrary
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -16,6 +17,10 @@ class AdminHomeFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_admin_home, container, false)
 
+        val addBookButton: Button = view.findViewById(R.id.add_book_button)
+        addBookButton.setOnClickListener {
+
+        }
         return view
     }
 }
---
+++ app/src/main/java/com/example/nushlibrary/Book.kt
@@ -0,0 +1,4 @@
+package com.example.nushlibrary
+
+data class Book(val author: String, val title: String, val description: String, val genre: String, val number: Int) {
+}
